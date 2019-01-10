182318f8f53add8346f8763c4aa13de7f5f84f72
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-10 15:28:56 -0600

    Doing things

--- linkedlist.js
+++ linkedlist.js
@@ -1,30 +1,51 @@
 const Node = require("./node");
+class Node {
+  constructor(data = null, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
 
-class LinkedList { 
-    constructor() { 
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
-    } 
-  
-    add (data) {
-      const newNode = new Node(data);
-      if (this.length === 0) {
-          this.head = this.tail = newNode;
-      } else {  
-          newNode.prev = this.tail;
-          this.tail.next = newNode;
-          this.tail = newNode;
-      }
-      this.length++;
-    } 
-    // get() 
-    indexOf (node) {
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
 
-    }  
-    // insertAt()
-    // removeAt()
+  add(data) {
+    const newNode = new Node(data);
+    if (this.length === 0) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 
-} 
+  indexOf(value) {
+    let currentNode = this.head;
+    let count = 0;
 
+    return currentNode.data;
+  }
+
+  get(n, node) {
+    if (this.length === 0) return null;
+    if (n === 0) return node;
+    else this.get(--n, node.next);
+  }
+
+  // insertAt()
+  // removeAt()
+}
+const myList = new LinkedList();
+myList.add(1);
+myList.add(2);
+myList.add(3);
+console.log(myList.get(2, 1));
 module.exports = LinkedList;
